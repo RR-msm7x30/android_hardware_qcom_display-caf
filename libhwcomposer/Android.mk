0ac5674da4d77c32cf18d7e499771f0a17421b3f
Călin Neamţu <nilac8991@Gmail.com>
2015-05-25 13:51:44 +0200

    Fixed compilation errors

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,8 +12,8 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia libvirtual libskia
-LOCAL_CFLAGS += -Os
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+LOCAL_CFLAGS += -Os
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
